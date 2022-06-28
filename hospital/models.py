ae8d524188850057c71f428903d5657eb25b4eb0
Mubin-Hardy <64096481+Mubin-Hardy@users.noreply.github.com>
2022-06-28 15:27:28 +0530

    Add files via upload

---
+++ hospital/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+departments=[('Cardiologist','Cardiologist'),
+('Dermatologists','Dermatologists'),
+('Emergency Medicine Specialists','Emergency Medicine Specialists'),
+('Allergists/Immunologists','Allergists/Immunologists'),
+('Anesthesiologists','Anesthesiologists'),
+('Colon and Rectal Surgeons','Colon and Rectal Surgeons')
+]
+class Doctor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/DoctorProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=True)
+    department= models.CharField(max_length=50,choices=departments,default='Cardiologist')
+    status=models.BooleanField(default=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return "{} ({})".format(self.user.first_name,self.department)
+
+
+
+class Patient(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/PatientProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    symptoms = models.CharField(max_length=100,null=False)
+    assignedDoctorId = models.PositiveIntegerField(null=True)
+    admitDate=models.DateField(auto_now=True)
+    status=models.BooleanField(default=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return self.user.first_name+" ("+self.symptoms+")"
+
+
+class Appointment(models.Model):
+    patientId=models.PositiveIntegerField(null=True)
+    doctorId=models.PositiveIntegerField(null=True)
+    patientName=models.CharField(max_length=40,null=True)
+    doctorName=models.CharField(max_length=40,null=True)
+    appointmentDate=models.DateField(auto_now=True)
+    description=models.TextField(max_length=500)
+    status=models.BooleanField(default=False)
+
+
+
+class PatientDischargeDetails(models.Model):
+    patientId=models.PositiveIntegerField(null=True)
+    patientName=models.CharField(max_length=40)
+    assignedDoctorName=models.CharField(max_length=40)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=True)
+    symptoms = models.CharField(max_length=100,null=True)
+
+    admitDate=models.DateField(null=False)
+    releaseDate=models.DateField(null=False)
+    daySpent=models.PositiveIntegerField(null=False)
+
+    roomCharge=models.PositiveIntegerField(null=False)
+    medicineCost=models.PositiveIntegerField(null=False)
+    doctorFee=models.PositiveIntegerField(null=False)
+    OtherCharge=models.PositiveIntegerField(null=False)
+    total=models.PositiveIntegerField(null=False)
+
+
+class covidcase(models.Model):
+    date = models.DateField(max_length=200,null=True)
+    #day = models.CharField(max_length=200,null=True)
+    district = models.CharField(max_length=40,null=True)
+    covidcases = models.CharField(max_length=40,null=True)
+    numberofvaccination=models.CharField(max_length=40,null=True)
+class Survey(models.Model):
+    cat = (('Yes','Yes'),('no','no'))
+    self_employed=models.CharField(max_length=50,choices=cat)
+    lost_job=models.CharField(max_length=50,choices=cat)
+    health_issues=models.CharField(max_length=50,choices=cat)
+    physical_health=models.CharField(max_length=50,choices=cat)
+    friends=models.CharField(max_length=50,choices=cat)
+    relatives=models.CharField(max_length=50,choices=cat)
+    salary_cuts=models.CharField(max_length=50,choices=cat)
+    exercise=models.CharField(max_length=50,choices=cat)
+    missed_daily_routines=models.CharField(max_length=50,choices=cat)
+
+    
